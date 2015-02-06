2b69609d76d4c59f354d7ad61d8b73b3696daed9
ldb358 <ldb358@gmail.com>
2015-02-06 10:11:39 -0800

    can now extract hog descriptors and have them to a file

--- hog/trainer.cpp
+++ hog/trainer.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 FileReader::FileReader(char *filename){
     myfile.open(filename);
-    img = new char[200];
+    img = new char[400];
     char_count = 0;
     line_count = 0;
     image_count = 0;
@@ -23,6 +23,9 @@ char *FileReader::nextimg(){
         c = line[char_count];
         char_count++;
     }
+    //zero the image path to prevent wierd left over chars
+    bzero(img, 400);
+    line[0] = '\0';
     line.copy(img, char_count-start-1, start); 
     return img;
 }
@@ -46,7 +49,8 @@ int FileReader::is_open(){
 }
 
 
-void calculateFeaturesFromInput(char* imageFilename, vector<float>& featureVector, HOGDescriptor& hog) 
+void calculateFeaturesFromInput(char* imageFilename, vector<float>& featureVector,
+        HOGDescriptor& hog) 
 {
         Mat image;
         image = imread(imageFilename);
@@ -55,13 +59,19 @@ void calculateFeaturesFromInput(char* imageFilename, vector<float>& featureVecto
             cout << "Image " << imageFilename << " Empty, skipping." << endl;
             return;
         }
-        waitKey(0);
+        if (image.cols != hog.winSize.width || image.rows != hog.winSize.height) {
+            featureVector.clear();
+            cout << imageFilename << " Couldn't use for trainging since the dimensions are wrong" << endl;
+            return;
+        }
+        hog.compute(image, featureVector); 
+        image.release();
 }
 
 
 int main(int argc, char**argv){
-    if(argc < 2){
-        cout << "You need to provide file contain a list of image to use for training" 
+    if(argc < 3){
+        cout << "You need to provide file contain a list of image to use for training, and the name of the file to use as a model" 
             << endl;
         return -1;
     }
@@ -70,13 +80,27 @@ int main(int argc, char**argv){
         cout << "File couldn't be opened... exiting.";
         return 1;
     }
+    // save our descriptors in the object(since we are only calculating one at a time
+    HOGDescriptor desc;
+    string tmpf = "model.temp";
+    //output file to save the model to
+    fstream outfile;
+    outfile.open(tmpf.c_str(), ios::out);
+    //instructions for training
+    outfile << "# Use this file to train, e.g. SVMlight by issuing $ svm_learn -i 1 -a weights.txt " << argv[2] << endl;
     while(file.has_next()){ 
         char *fname = file.nextimg(); 
         cout << (file.is_pos()? "Pos" : "Neg") << ":" << fname << endl;
         vector<float> features;
-        HOGDescriptor desc;
-        calculateFeaturesFromInput(fname, features, desc);
-
+        calculateFeaturesFromInput(fname, features, desc);  
+        if(!features.empty()){
+            //output +1(pos) or -1(neg) to class file
+            outfile << (file.is_pos()? "+1" : "-1");
+            for(unsigned int feature = 0; feature < features.size(); ++feature){
+                outfile << " " << feature+1 << ":" << features.at(feature);
+            }
+            outfile << endl;
+        }
     }
     
 }
